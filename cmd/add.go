5d7d5c89a8c65e854ece0d605494d4c70ec0ec8c
Alex Bazhenov <vorobushec13@gmail.com>
2024-03-04 01:43:52 +0300

    New update

---
+++ cmd/add.go
@@ -0,0 +1,21 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "A brief description of your command",
+	// Run: func(cmd *cobra.Command, args []string) {
+	// 	fmt.Println("add called")
+	// },
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+}
