0647e477d57e36e1c7977b423ba1f924cf26fe36
Alex Bazhenov <vorobushec13@gmail.com>
2024-03-10 01:13:38 +0300

    New update

--- db/tasks.go
+++ db/tasks.go
@@ -2,15 +2,15 @@ package db
 
 import "github.com/boltdb/bolt"
 
-var taskBucket = []byte("tasks")
-var db *bolt.DB
+var taskBucket = []byte("tasks") // создаём новый бакет
+var db *bolt.DB                  // создаём переменную типа нашей базы данных
 
-type Task struct {
+type Task struct { // создаём структуру нашей БД, состоящей из ключа и его значения
 	Key   int
 	Value string
 }
 
-func Init(dbPath string) error {
+func Init(dbPath string) error { // инициализируем БД
 	var err error
 	db, err = bolt.Open(dbPath, 0600, &bolt.Options{})
 	if err != nil {
@@ -20,4 +20,12 @@ func Init(dbPath string) error {
 		_, err := tx.CreateBucketIfNotExists(taskBucket)
 		return err
 	})
+}
+
+func CreateTask(task string) (int, error) {
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		id64, _ := b.NextSequence()
+	})
+	return 0, nil
 }
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "list/cmd"
+import (
+	"fmt"
+	"list/cmd"
+	"list/db"
+	"path/filepath"
+
+	"github.com/mitchellh/go-homedir"
+)
 
 func main() {
+	home, _ := homedir.Dir() // Хранение бд в домашнем каталоге пользователя
+	dbPath := filepath.Join(home, "tasks.db")
+	err := db.Init(dbPath)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("asdasd")
 	cmd.Execute()
 }
